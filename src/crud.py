24cbd91fb199d25e9a9384d1754a1732e2064f9e
nodax <ioann.ishenko.2003@gmail.com>
2024-04-03 19:27:35 +0400

    добавлена функция для получения комиксов по полю; исправлена ошибка при создании пользователя.

--- src/crud.py
+++ src/crud.py
@@ -15,6 +15,10 @@ def get_user_by_login(db: Session, login: str):
     return db.query(models.User).filter(models.User.login == login).first()
 
 
+def get_comic_book_by_attribute(db: Session, skip: int = 0, limit: int = 100, **kwargs):
+    return db.query(models.ComicBook).filter_by(**kwargs).offset(skip).limit(limit).all()
+
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
@@ -22,8 +26,9 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 def create_user(db: Session, user: schemas.UserCreate):
     # TODO: хешировать пароль
     fake_hashed_password = user.password + "notreallyhashed"
+    del user.password
     
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = models.User(**user.model_dump(), hashed_password=fake_hashed_password)
     
     db.add(db_user)
     db.commit()
