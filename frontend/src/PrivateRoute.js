6b5d6c43e6661460daf29e17769b669f045add8f
ApelsinDev <nik_zhur_serg@mail.ru>
2024-06-19 14:00:22 +0400

    кpr

---
+++ frontend/src/PrivateRoute.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { UserContext } from './context/UserContext';
+
+const PrivateRoute = () => {
+  const [token] = useContext(UserContext);
+
+  // Если токен отсутствует, перенаправляем на страницу входа
+  return token ? <Outlet /> : <Navigate to="/" replace />;
+};
+
+export default PrivateRoute;
